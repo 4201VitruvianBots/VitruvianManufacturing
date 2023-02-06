8c02c47f572803aae914a1f360106cb94312db03
SKYED <skyehddouglas@gmail.com>
2023-02-05 16:10:01 -0800

    finished

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,17 @@
-from flask import Flask  
-app = Flask (__name__)  
-  
-@app.route("/")  
-def home():  
-    return "Hello my name is Skye"
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello world!"
+
+
+@app.route("/api/parts", methods=["GET"])
+def parts():
+    return "request.form"
+
+
+@app.route("/api/parts", methods=["POST"])
+def newParts():
+    return "Hello!"
