6cd8ce06b617554ea13a2a9b7cf2b1eefadf9148
SkyeD <skyedouglas32@gmail.com>
2023-02-07 18:24:11 -0800

    tabs are aligned

--- Front-End/tab/src/App.js
+++ Front-End/tab/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [value,setValue] = React.useState(0)
 
   const actArray = []
-  for(let i=0;i<4;i++){
+  for(let i=0;i<10;i++){
     if (i===value){
       actArray.push('btn active')
     }
@@ -17,19 +17,16 @@ function App() {
 
   return (
     <div className="App">
-      <ul style={{listStyle:'left', marginLeft:'30%'}}>
-      <li style={{float:'left'}}><button type='button' className={actArray[8]} onClick={()=>{setValue(0)}}>Sign In</button></li>
-        <li style={{float:'left'}}><button type='button' className={actArray[0]} onClick={()=>{setValue(1)}}>Drivetrain</button><h1></h1></li>
-        <li style={{float:'left'}}><button type='button' className={actArray[1]} onClick={()=>{setValue(2)}}>Bumpers</button></li>
-        <li style={{float:'left'}}><button type='button' className={actArray[2]}  onClick={()=>{setValue(3)}}>Elevator</button></li>
-        <li style={{float:'left'}}><button type='button' className={actArray[3]} onClick={()=>{setValue(4)}}>Intake</button></li>
-        <li style={{float:'left'}}><button type='button' className={actArray[4]} onClick={()=>{setValue(5)}}>Auxiliary Systems</button></li>
-        <li style={{float:'left'}}><button type='button' className={actArray[5]} onClick={()=>{setValue(6)}}>Forebar</button></li>
-        <li style={{float:'left'}}><button type='button' className={actArray[6]} onClick={()=>{setValue(7)}}>3D Print</button></li>
-        <li style={{float:'left'}}><button type='button' className={actArray[7]} onClick={()=>{setValue(8)}}>Intake</button></li>
-
-
-
+      <ul style={{listStyle:'none'}}>
+        <li style={{float:'left'}}><button type='button' className={actArray[0]} onClick={()=>{setValue(0)}}>SignIn</button></li>
+        <li style={{float:'left'}}><button type='button' className={actArray[1]} onClick={()=>{setValue(1)}}>Drivetrain</button></li>
+        <li style={{float:'left'}}><button type='button' className={actArray[2]}  onClick={()=>{setValue(2)}}>Bumpers</button></li>
+        <li style={{float:'left'}}><button type='button' className={actArray[3]} onClick={()=>{setValue(3)}}>Elevator</button></li>
+        <li style={{float:'left'}}><button type='button' className={actArray[4]} onClick={()=>{setValue(4)}}>Intake</button></li>
+        <li style={{float:'left'}}><button type='button' className={actArray[5]} onClick={()=>{setValue(5)}}>Auxiliary Systems</button></li>
+        <li style={{float:'left'}}><button type='button' className={actArray[6]} onClick={()=>{setValue(6)}}>Forebar</button></li>
+        <li style={{float:'left'}}><button type='button' className={actArray[7]} onClick={()=>{setValue(7)}}>3D Print</button></li>
+        <li style={{float:'left'}}><button type='button' className={actArray[8]} onClick={()=>{setValue(8)}}>Save Page</button></li>
       </ul>
       <br/><br/>
       <div>
