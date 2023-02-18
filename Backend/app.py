fdc035aabb428ffa0426fd0b8b8eefa62b52500d
SkyeD <skyedouglas32@gmail.com>
2023-02-18 14:51:57 -0800

    WIP #16

--- Backend/app.py
+++ Backend/app.py
@@ -19,5 +19,12 @@ def newParts():
     return "Hello!"
 
 
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['my_file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+
 if __name__ == "__main__":
     app.run()
--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -29,7 +29,8 @@ function App() {
             </ul>
             <br /><br />
             <div>
-                <form action="http://127.0.0.1:5000/api/parts" method="POST">
+                <form action="http://127.0.0.1:5000/api/parts" method="POST"
+                    enctype="multipart/form-data">
                     <Choosepage value={value} />
                 </form>
             </div>
