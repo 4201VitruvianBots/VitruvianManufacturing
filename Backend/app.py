23b3e2b98780488c650df33c71db97e98c80f948
SKYED <skyehddouglas@gmail.com>
2023-02-18 16:59:17 -0800

    fix

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 
@@ -19,12 +20,13 @@ def newParts():
     return "Hello!"
 
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['my_file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+    if request.method == 'POST':
+        f = request.files['my_file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+
 
 if __name__ == "__main__":
     app.run()
