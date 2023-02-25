e7d181a402e45323fafc7c6b0b3d963211622fc6
SKYED <skyehddouglas@gmail.com>
2023-02-24 17:50:52 -0800

    WIP

--- Front-End/src/PageM.js
+++ Front-End/src/PageM.js
@@ -59,7 +59,7 @@ function DrivetrainDataTable(props) {
 
                 <p className="selection-label" id="Drive-Train-Data-Table">Drivetrain Data Tables</p>
                 <div className="textArea">
-                    <table border="1" width="100%">
+                    <table border="" width="200%">
                         <tr>
                             <th>Mechanism</th>
                             <th>Number Of Parts</th>
@@ -130,7 +130,7 @@ function DrivetrainDataTable(props) {
 
                 </div>
             </div>
-        </Page>
+        </Page >
     )
 }
 
@@ -142,7 +142,7 @@ function BumpersDataTable(props) {
 
                 <p className="selection-label" id="Bumpers-Data-Table">Bumpers Data Tables</p>
                 <div className="textArea">
-                    <table border="1" width="100%">
+                    <table border="1" width="200%">
                         <tr>
                             <th>Mechanism</th>
                             <th>Number Of Parts</th>
@@ -225,7 +225,7 @@ function ElevatorDataTable(props) {
 
                 <p className="selection-label" id="Elevator-Data-Table">Elevator Data Tables</p>
                 <div className="textArea">
-                    <table border="1" width="100%">
+                    <table border="1" width="200%">
                         <tr>
                             <th>Mechanism</th>
                             <th>Number Of Parts</th>
@@ -308,7 +308,7 @@ function IntakeDataTable(props) {
 
                 <p className="selection-label" id="Intake-Data-Table">Intake Data Tables</p>
                 <div className="textArea">
-                    <table border="1" width="100%">
+                    <table border="1" width="200%">
                         <tr>
                             <th>Mechanism</th>
                             <th>Number Of Parts</th>
@@ -391,7 +391,7 @@ function AuxiliarySystemsDataTable(props) {
 
                 <p className="selection-label" id="Auxiliary-Systems-Data-Table">Auxiliary Systems Data Tables</p>
                 <div className="textArea">
-                    <table border="1" width="100%">
+                    <table border="1" width="200%">
                         <tr>
                             <th>Mechanism</th>
                             <th>Number Of Parts</th>
@@ -459,7 +459,6 @@ function AuxiliarySystemsDataTable(props) {
                             <td>Hello</td>
                         </tr>
                     </table>
-
                 </div>
             </div>
         </Page>
@@ -471,10 +470,9 @@ function ForebarDataTable(props) {
         <Page selected={props.selected} id="ForebarDataTable">
             <div>
                 <br></br>
-
                 <p className="selection-label" id="Forebar-Data-Table">Forebar Data Tables</p>
                 <div className="textArea">
-                    <table border="1" width="100%">
+                    <table border="1" width="200%">
                         <tr>
                             <th>Mechanism</th>
                             <th>Number Of Parts</th>
@@ -557,7 +555,7 @@ function ThreeDPrintDataTable(props) {
 
                 <p className="selection-label" id="Three-D-Print-Data-Table">3D Print Data Tables</p>
                 <div className="textArea">
-                    <table border="1" width="100%">
+                    <table border="1" width="200%">
                         <tr>
                             <th>Mechanism</th>
                             <th>Number Of Parts</th>
