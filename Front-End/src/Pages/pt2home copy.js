dae364d038bb208d6a60ac5bcde448feb10735f0
SkyeD <skyedouglas32@gmail.com>
2023-03-23 14:37:37 -0700

    WIP

---
+++ Front-End/src/Pages/pt2home copy.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { AuxiliarySystems, AuxiliarySystemsPhotos, BumperPhotos, Bumpers, Drivetrain, DrivetrainPhotos, Elevator, ElevatorPhotos, Forebar, ForebarPhotos, Intake, IntakePhotos, SavePage, SignIn, ThreeDPrint, ThreeDPrintPhotos } from '../NewPartApp';
+import "./app.css";
+
+const NewPartApp = () => {
+    const [value, setValue] = React.useState(0)
+
+    const actArray = []
+    for (let i = 0; i < 10; i++) {
+        if (i === value) {
+            actArray.push('btn active')
+        }
+        else {
+            actArray.push('btn')
+        }
+    }
+    return (
+
+        <div className="App">
+            <ul style={{ listStyle: 'none' }}>
+                <li style={{ float: 'left' }}><button type='button' className={actArray[0]} onClick={() => { setValue(0) }}>Begin</button></li>
+                <li style={{ float: 'left' }}><button type='button' className={actArray[1]} onClick={() => { setValue(1) }}>Upload File</button></li>
+                <li style={{ float: 'left' }}><button type='button' className={actArray[2]} onClick={() => { setValue(2) }}>Part Sumbmission</button></li>
+                <li style={{ float: 'left' }}><button type='button' className={actArray[3]} onClick={() => { setValue(3) }}>Sumbmission</button></li>
+            </ul>
+            <br /><br />
+            <div>
+                <form action="http://127.0.0.1:5000/api/homept3" method="POST"
+                    enctype="multipart/form-data">
+                    <Choosepage value={value} />
+                </form>
+            </div>
+        </div>
+
+    );
+}
+
+function Choosepage(props) {
+    if (props.value === 0) {
+        return (<SignIn />
+        );
+    }
+
+    if (props.value === 1) {
+        return (<><Drivetrain /><DrivetrainPhotos /> <SavePage /></>
+        );
+    }
+
+
+    if (props.value === 2) {
+        return (<><Bumpers /><BumperPhotos /><SavePage /></>
+        );
+    }
+    if (props.value === 3) {
+        return (<><Elevator /><ElevatorPhotos /><SavePage /></>
+        );
+    }
+    if (props.value === 4) {
+        return (<><Intake /><IntakePhotos /><SavePage /></>
+        );
+    }
+    if (props.value === 5) {
+        return (<><AuxiliarySystems /><AuxiliarySystemsPhotos /><SavePage /></>
+        )
+    }
+    if (props.value === 6) {
+        return (<><Forebar /><ForebarPhotos /><SavePage /></>
+        );
+    }
+    if (props.value === 7) {
+        return (<><ThreeDPrint /><ThreeDPrintPhotos /><SavePage /></>
+        );
+    }
+}
+
+export default NewPartApp;
