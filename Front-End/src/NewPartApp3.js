1128710ff6a8d85741f16756b6421eda26c36e29
SKYED <skyehddouglas@gmail.com>
2023-03-28 16:39:37 -0700

    added tailwindcss

--- Front-End/src/NewPartApp3.js
+++ Front-End/src/NewPartApp3.js
@@ -99,7 +99,7 @@ function VitruvianManfacturing(props) {
                 </div>
                 <br></br>
                 <br></br>
-                <input type="submit" value="Submit"/>
+                <input type="submit" value="Submit" />
             </div>
 
             <div className="colummtest">
@@ -132,7 +132,7 @@ function VitruvianManfacturing(props) {
                     <input type="checkbox" id="N/A" name="N/A" />
                     <label for="N/A">N/A</label>
                     <br></br>
-            </div>
+                </div>
             </div>
         </Page>
     );
@@ -145,22 +145,22 @@ function UploadFile(props) {
         <Page selected={props.selected} id="UploadFile">
             <div>
                 <br></br>
-                <div className="BigBox">
-                    <br></br>
+                <br></br>
+                <div className="hello">
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" /> 
-  <link rel="stylesheet" href="App.css" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
+                    <link rel="stylesheet" href="App.css" />
 
-  <div class="container1">
-    <input type="file1" id="file-input1" multiple />
-    <label for="file-input1">
-      <i class="fa-solid fa-arrow-up-from-bracket"></i>
-      &nbsp; Choose Files To Upload
-    </label>
-    <div id="num-of-files1">No Files Choosen</div>
-    <ul id="files-list1"></ul>
-  </div>
-  <script src="script.js"></script>
+                    <div class="container1">
+                        <input type="file1" id="file-input1" multiple />
+                        <label for="file-input1">
+                            <i class="fa-solid fa-arrow-up-from-bracket"></i>
+                            &nbsp; Choose Files To Upload
+                        </label>
+                        <div id="num-of-files1">No Files Choosen</div>
+                        <ul id="files-list1"></ul>
+                    </div>
+                    <script src="script.js"></script>
                 </div>
             </div>
         </Page >
@@ -183,74 +183,9 @@ function Part(props) {
 
 function UploadComplete(props) {
     return (
-        <Page selected={props.selected} id="Bumpers">
-            <div>
-                <p className="section-label" id="Bumpers">Bumpers</p>
-                <div className="textArea">
-                    <input type="text" id="NumberOfParts" name="bumpers_NumberOfParts" placeholder="Number of parts that need to be machined" className="text-input" required="required" />
-                    <br></br>
-                    <label className="SizeOfPart" htmlFor="SizeOfPart"><strong>Size Of Part</strong>  </label>
-                    <select name="bumpers_SizeOfPart" id="SizeOfPart" defaultValue="" className="form-control" required >
-                        <option value="" className="Placeholder" disabled>Click Here!</option>
-                        <option value="1*1">1x1 </option>
-                        <option value="1x2">1x2</option>
-                        <option value="1.5x1.5">1.5x1.5</option>
-                        <option value="2x2">2x2</option>
-                        <option value="L-Bracket ">L-Bracket</option>
-                        <option value="Shaft">Shaft</option>
-                        <option value="Nothing">Nothing</option>
-                    </select>
-                    <br></br>
-                    <label className="TypeOfStock" htmlFor="TypeOfStock"><strong>Type of Stock</strong> </label>
-                    <select name="bumpers_TypeOfStock" id="TypeOfPart" defaultValue="" className="form-control" required >
-                        <option value="" className="Placeholder" disabled>Click Here!</option>
-                        <option value="Extrusion">Extrusion</option>
-                        <option value="Plate">Plate</option>
-                        <option value="Hex Shaft">Hex Shaft</option>
-                        <option value="Shaft">Shaft</option>
-                        <option value="3D-Print">3D-Print</option>
-                        <option value="Nothing">Nothing</option>
-                    </select>
-                    <br></br>
-                    <label className="Thickness (Extrusion)" htmlFor="Thickness (Extrusion)"><strong>Thickness (Extrusion)</strong> </label>
-                    <select name="bumpers_Thickness(Extrusion)" id="Thickness (Extrusion)" defaultValue="" className="form-control" required >
-                        <option value="" className="Placeholder" disabled>Click Here!</option>
-                        <option value="1/16">1/16</option>
-                        <option value="1/8">1/8</option>
-                        <option value="Nothing">Nothing</option>
-                    </select>
-                    <label className="Thickness (Plate)" htmlFor="Thickness (Plate)"><strong>Thickness (Plate)</strong> </label>
-                    <select name="bumpers_Thickness(Plate)" id="Thickness (Plate)" defaultValue="" className="form-control" required >
-                        <option value="" className="Placeholder" disabled>Click Here!</option>
-                        <option value="1/16">1/16</option>
-                        <option value=".09">.09</option>
-                        <option value="1/8">1/8</option>
-                        <option value="1/4">1/4</option>
-                        <option value="3/8">3/8</option>
-                        <option value="3/4">3/4</option>
-                        <option value="Nothing">Nothing</option>
-                    </select>
-                    <label className="Material (Plate)" htmlFor="Material (Plate)"><strong>Material (Plate) </strong> </label>
-                    <select name="bumpers_Material(Plate)" id="Material  (Plate)" defaultValue="" className="form-control" required >
-                        <option value="" className="Placeholder" disabled>Click Here!</option>
-                        <option value="Aluminum">Aluminum</option>
-                        <option value="Polycarbonate">Polycarbonate</option>
-                        <option value="Wood">Wood</option>
-                        <option value="PETG">PETG</option>
-                        <option value="PLA">PLA</option>
-                        <option value="ONYX">ONYX</option>
-                        <option value="Nothing">Nothing</option>
-                    </select>
-                    <br></br>
-                    <input type="text" id="LengthInInches" name="bumpers_LengthInInches" placeholder="Length in Inches" className="text-input" required="required" />
-                    <br></br>
-                    <input type="text" id="PartNumber" name="bumpers_PartNumber" placeholder="Part Number" className="text-input" required="required" />
-                    <br></br>
-                    <br></br>
-                    <h4>When do you need the part done by?</h4>
-                </div>
-            </div>
-        </Page>
+        <div className="BigBox1">
+            Upload Complete
+        </div>
     );
 }
 
