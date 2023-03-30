529e06d44f59eb79ce13741b8ff0b8024526a301
SkyeD <skyedouglas32@gmail.com>
2023-03-30 09:59:08 -0700

    wip

--- Front-End/src/Pages/pt2homecopy.js
+++ Front-End/src/Pages/pt2homecopy.js
@@ -21,8 +21,6 @@ const NewPartApp3 = () => {
                 <li style={{ float: 'left' }}><button type='button' className={actArray[0]} onClick={() => { setValue(0) }}>Begin</button></li>
                 <li style={{ float: 'left' }}><button type='button' className={actArray[1]} onClick={() => { setValue(1) }}>Upload File</button></li>
                 <li style={{ float: 'left' }}><button type='button' className={actArray[2]} onClick={() => { setValue(2) }}>Part Sumbmission</button></li>
-                <li style={{ float: 'left' }}><button type='button' className={actArray[3]} onClick={() => { setValue(3) }}>Part</button></li> 
-                 <li style={{ float: 'left' }}><button type='button' className={actArray[4]} onClick={() => { setValue(4) }}>Upload Complete</button></li>
             </ul>
             <br /><br />
             <div>
@@ -51,11 +49,7 @@ function Choosepage(props) {
         return (<UploadFile />
         );
     }
-    if (props.value === 2) {
-        return (<Part />
-        );
-    }
-    if (props.value === 3) {
+   (props.value === 2) {
         return (<UploadComplete />
         );
     }
