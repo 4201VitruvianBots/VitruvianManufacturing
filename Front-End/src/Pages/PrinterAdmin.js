824b76b871243635173378c8f2be609c6a836dfd
SkyeD <skyedouglas32@gmail.com>
2023-09-10 16:46:21 -0700

    Wip
    Deleted app.css and making 1 App.css. I am working on changing the files rn

--- Front-End/src/Pages/PrinterAdmin.js
+++ Front-End/src/Pages/PrinterAdmin.js
@@ -1,49 +1,49 @@
-import "./app.css";
-import React from 'react';
-import { PrinterStatusDataTable } from '../PageP';
-import { SavePage, SignIn } from '../Pages';
+// import "./app.css";
+// import React from 'react';
+// import { PrinterStatusDataTable } from '../PageP';
+// import { SavePage, SignIn } from '../Pages';
 
-function PrinterStatusDataPage() {
-    const [value, setValue] = React.useState(0)
+// function PrinterStatusDataPage() {
+//     const [value, setValue] = React.useState(0)
 
-    const actArray = []
-    for (let i = 0; i < 10; i++) {
-        if (i === value) {
-            actArray.push('btn active')
-        }
-        else {
-            actArray.push('btn')
-        }
-    }
+//     const actArray = []
+//     for (let i = 0; i < 10; i++) {
+//         if (i === value) {
+//             actArray.push('btn active')
+//         }
+//         else {
+//             actArray.push('btn')
+//         }
+//     }
 
-    return (
-        <div className="App">
-            <ul style={{ listStyle: 'none' }}>
-                {/* <li style={{ float: 'left' }}><button type='button' className={actArray[0]} onClick={() => { setValue(0) }}>SignIn</button></li> */}
-                <li style={{ float: 'left' }}><button type='button' className={actArray[0]} onClick={() => { setValue(0) }}>Printer Status Admin</button></li>
-            </ul>
-            <br /><br />
-            <div>
-                <form action="http://127.0.0.1:5000/api/PrinterAdmin" method="POST"
-                    enctype="multipart/form-data">
-                    <Choosepage value={value} />
-                </form>
-            </div>
-        </div>
+//     return (
+//         <div className="App">
+//             <ul style={{ listStyle: 'none' }}>
+//                 {/* <li style={{ float: 'left' }}><button type='button' className={actArray[0]} onClick={() => { setValue(0) }}>SignIn</button></li> */}
+//                 <li style={{ float: 'left' }}><button type='button' className={actArray[0]} onClick={() => { setValue(0) }}>Printer Status Admin</button></li>
+//             </ul>
+//             <br /><br />
+//             <div>
+//                 <form action="http://127.0.0.1:5000/api/PrinterAdmin" method="POST"
+//                     enctype="multipart/form-data">
+//                     <Choosepage value={value} />
+//                 </form>
+//             </div>
+//         </div>
 
-    );
-}
-function Choosepage(props) {
-    // if (props.value === 0) {
-    //     return (<SignIn />
-    //     );
-    // }
+//     );
+// }
+// function Choosepage(props) {
+//     // if (props.value === 0) {
+//     //     return (<SignIn />
+//     //     );
+//     // }
 
-    if (props.value === 0) {
-        return (<><PrinterStatusDataTable /><SavePage /></>
-        );
-    }
+//     if (props.value === 0) {
+//         return (<><PrinterStatusDataTable /><SavePage /></>
+//         );
+//     }
 
-}
+// }
 
-export default PrinterStatusDataPage;
+// export default PrinterStatusDataPage;
